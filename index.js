29508da9fa3b6f811ed42178270cc85d685fb783
Akseth314 <akankshaseth314@gmail.com>
2024-09-07 12:59:17 +0530

    fu

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -9,19 +8,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/",(req,res) => {
-    res.send(`<h1>This is my API</h1>`);
-})
+app.get("/", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.send(`<h1>This is my API</h1>`);
+});
+
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Could not connect to MongoDB', error));
+
 
 app.use('/api/aqi', aqiRoutes);
 
